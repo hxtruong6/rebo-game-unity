03306a14894922a7b0182b4989556c27e4ed9dec
Truong98 <ngoctruong9x.inc@gmail.com>
2019-10-29 13:08:22 +0700

    Recreate SceneGame
    add Hero, HealthBar, LevelBar

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public int jumpCount;
+    private const int maxJumpCount = 2;
+    public LevelBar level;
+    public HealthBar health;
+
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        speed = 7f;
+        jumpSpeed = 200f;
+        jumpCount = 0;
+      
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            SetRun(true);
+            
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                sprite.flipX = false;
+
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            }
+            else
+            {
+                sprite.flipX = true;
+
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            }
+
+        }
+        else
+        {
+            SetRun(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (jumpCount < maxJumpCount)
+            {
+                jumpCount++;
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed));
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpCount = 0;
+    }
+
+    private void SetRun(bool value)
+    {
+        GetComponent<Animator>().SetBool("isRunning", value);
+    }
+
+    private void SetShoot(bool value)
+    {
+        GetComponent<Animator>().SetBool("isShooting", value);
+    }
+}
---
+++ Assets/Scripts/LevelBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBar : MonoBehaviour
+{
+    public Sprite level0, level1, level2, level3, level4, level5, level6;
+    public const int maxLevel = 6;
+    public int currentLevel;
+    public Vector2 levelBarPos;
+
+    private Sprite[] levels;
+
+    void Start()
+    {
+        levelBarPos = new Vector2(50, 100);
+        currentLevel = 0;
+        levels = new Sprite[maxLevel + 1] { level0, level1, level2, level3, level4, level5, level6 };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Find("Bar").GetComponent<SpriteRenderer>().sprite = levels[currentLevel];
+    }
+
+    public float GetDamage()
+    {
+        return currentLevel * 10;
+    }
+}
