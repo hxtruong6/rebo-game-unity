177087345b0480cb12e26222fe0c3e8fd8240a35
hxtruong <hoangxuantruong98@gmail.com>
2019-12-01 11:56:07 +0700

    feat: create mission 1 and load scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string ScenceName = "Missions";
+    public void LoadMissionsScence()
+    {
+        StartCoroutine(LoadYourAsyncScene(ScenceName));
+    }
+
+    IEnumerator LoadYourAsyncScene(string scence)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scence);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
